8da205998255ede085522bfd06b20d2d9d9ed683
KhoiHai <162028733+KhoiHai@users.noreply.github.com>
2024-06-19 21:12:42 +0700

    Finish writing all hpp cpp files for each sorting algorithm

---
+++ src/sorting/heap_sort.cpp
@@ -0,0 +1,21 @@
+#include "heap_sort.hpp"
+
+void Hsort(int* arr, int n){
+    // cout << "hello world";
+}
+
+void Hsort_Count(int* arr, int n, int &count){
+    // cout << "hello world";
+}
+
+void Heap_Sort(int* arr, int n){
+    Hsort(arr, n);
+}
+
+/*
+From selection_sort.hpp
+*/
+void Heap_Sort_Count(int* arr, int n, int& count){
+    count = 0;
+    Hsort_Count(arr, n, count);
+}
---
+++ src/sorting/insertion_sort.cpp
@@ -0,0 +1,21 @@
+#include "insertion_sort.hpp"
+
+void Isort(int* arr, int n){
+    // cout << "hello world";
+}
+
+void Isort_Count(int* arr, int n, int &count){
+    // cout << "hello world";
+}
+
+void Insertion_Sort(int* arr, int n){
+    Isort(arr, n);
+}
+
+/*
+From selection_sort.hpp
+*/
+void Insertion_Sort_Count(int* arr, int n, int& count){
+    count = 0;
+    Isort_Count(arr, n, count);
+}
