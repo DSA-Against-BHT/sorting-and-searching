e078bf9c2118a2f4747e5c14f34ab1a1dfc940b2
Duy Le <duyla25clc@gmail.com>
2024-06-19 21:22:42 +0700

    testing sorting file

--- src/algo_mode.hpp
+++ src/algo_mode.hpp
@@ -1,6 +1,11 @@
-#include <iostream>
 #include <string>
-
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <iomanip>
+#include "function_call.hpp"
+#include "timer.hpp"
+#include "./generator/gen.hpp"
 using namespace std;
 
 void Handle_Command_1(string algo1, string givenInput, string outputParameter);
