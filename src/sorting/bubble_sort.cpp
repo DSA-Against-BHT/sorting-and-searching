9e0e7fe5a82b280ec54843772b0b4f0ec62e3b70
KhoiHai <162028733+KhoiHai@users.noreply.github.com>
2024-06-24 17:10:05 +0700

    Finish Bubble Sort

--- src/sorting/bubble_sort.cpp
+++ src/sorting/bubble_sort.cpp
@@ -1,19 +1,31 @@
 #include "bubble_sort.hpp"
 
 void Bsort(int* arr, int n){
-    // cout << "hello world";
+    for(int i = 1; i < n; i ++){
+        for(int j = 0; j < n - i; j ++){
+            if(arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        }
+    }
 }
 
 void Bsort_Count(int* arr, int n, int &count){
-    // cout << "hello world";
+    int i, j;
+    for(i = 1; ++ count && i < n; i ++){
+        for(j = 0; ++ count && j < n - i; j ++){
+            if(++ count && arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        }
+    }
 }
 
+/*
+This function is used to sort the array using bubble sort
+*/
 void Bubble_Sort(int* arr, int n){
     Bsort(arr, n);
 }
 
 /*
-From selection_sort.hpp
+This function is used to sort the array using bubble sort and count the number of comparisons
 */
 void Bubble_Sort_Count(int* arr, int n, int& count){
     count = 0;
