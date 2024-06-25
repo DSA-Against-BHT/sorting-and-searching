e2685f4b0584b847ff49c62784a0c69b2aed881e
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2024-06-25 14:05:35 +0700

    done the bubble sort

--- src/sorting/bubble_sort.cpp
+++ src/sorting/bubble_sort.cpp
@@ -1,17 +1,24 @@
 #include "bubble_sort.hpp"
 
 void Bsort(int* arr, int n){
-    for(int i = 1; i < n; i ++){
-        for(int j = 0; j < n - i; j ++){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
             if(arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
         }
     }
 }
 
-void Bsort_Count(int* arr, int n, long long& count){
-    // cout << "hello world";
+void Bsort_Count(int* arr, int n, int& count){
+    for(int i = 0; ++count && i < n - 1; i++){
+        for(int j = 0; ++count && j < n - i - 1; j++){
+            if(++count && arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        }
+    }
 }
 
+/*
+This function is used to sort the array using bubble sort
+*/
 void Bubble_Sort(int* arr, int n){
     Bsort(arr, n);
 }
@@ -19,7 +26,7 @@ void Bubble_Sort(int* arr, int n){
 /*
 This function is used to sort the array using bubble sort and count the number of comparisons
 */
-void Bubble_Sort_Count(int* arr, int n, long long& count){
+void Bubble_Sort_Count(int* arr, int n, int& count){
     count = 0;
     Bsort_Count(arr, n, count);
 }
