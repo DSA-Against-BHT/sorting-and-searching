a05866a6fcc5141d8a1520146b89c4874f5906f0
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2024-06-25 14:34:24 +0700

    done the bubble sort

--- src/sorting/bubble_sort.cpp
+++ src/sorting/bubble_sort.cpp
@@ -1,17 +1,24 @@
 #include "bubble_sort.hpp"
 
 void Bsort(int* arr, int n){
-    for(int i = 1; i < n; i ++){
-        for(int j = 0; j < n - i; j ++){
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < n - i; j++){
             if(arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
         }
     }
 }
 
 void Bsort_Count(int* arr, int n, long long& count){
-    // cout << "hello world";
+    for(int i = 1; ++count && i < n; i++){
+        for(int j = 0; ++count && j < n - i; j++){
+            if(++count && arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        }
+    }
 }
 
+/*
+This function is used to sort the array using bubble sort
+*/
 void Bubble_Sort(int* arr, int n){
     Bsort(arr, n);
 }
